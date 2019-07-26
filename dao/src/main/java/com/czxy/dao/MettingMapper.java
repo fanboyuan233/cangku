6c6cb3057cc4452ffe12ba5190aa751e533f8ad7
fanboyuan233 <53322840+fanboyuan233@users.noreply.github.com>
2019-07-26 11:13:46 +0800

    Update MettingMapper.java

--- dao/src/main/java/com/czxy/dao/MettingMapper.java
+++ dao/src/main/java/com/czxy/dao/MettingMapper.java
@@ -6,9 +6,5 @@ import tk.mybatis.mapper.common.Mapper;
 @org.apache.ibatis.annotations.Mapper
 public interface MettingMapper extends Mapper<Metting> {
 
-    @Select("select * from tab_metting where metting_id=#{mettingId}")
-    @Results({
-            @Result(property = "user",many = @Many(select = "com.czxy.dao.UserMapper.findUserByUsername"),column = "username")
-    })
-    public Metting findMettingByMid(@Param("mettingId")Integer mettingId);
+    
 }
